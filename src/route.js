647ce076bf41b2b276dd91a4e0f64767514af1ba
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2021-06-26 22:26:13 -0300

    Projeto Finalizado

--- src/route.js
+++ src/route.js
@@ -1,12 +1,20 @@
-const express = require('express'); /* Importando o express */
+const express = require('express') /* Importando o express */
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route  = express.Router() /* A constante 'route' guarda todas as funcionalidades de rotas que o express tem */
 
 /**** DEFININDO AS ROTAS ****/
 // Requisition e Response
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index) // Formato que o fomormulario de dentro da modal tem que passar a informação
 
 
 module.exports = route // Exportando a const route
--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ server.use(express.static("public")) /* Para usar o conteudo estático da págin
 
 server.set('views', path.join(__dirname, 'views')) /* Para indicar o caminho da pasta onde está o nosso projeto e junta "path.join". O dirname é para retornar o caminho/path para o diretório pai. Simplificando: .../rocketq/src/views */
 
+server.use(express.urlencoded({extended: true})) 
+
 server.use(route)
 
 server.listen(3000, () => console.log("RODANDO")) /* Passar uma porta ao server */
